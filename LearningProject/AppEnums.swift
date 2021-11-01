4f9e33b3b2f6969b2bf12ccaa7aa4fd016c069a9
Sukhmani Kaur <sukhmanikaur.appknit@gmail.com>
2021-11-01 18:03:44 +0530

    first commit

---
+++ LearningProject/AppEnums.swift
@@ -0,0 +1,8 @@
+//
+//  AppEnums.swift
+//  LearningProject
+//
+//  Created by MACBOOK on 01/11/21.
+//
+
+import Foundation
---
+++ LearningProject/HeaderCell.swift
@@ -0,0 +1,23 @@
+//
+//  HeaderCell.swift
+//  LearningProject
+//
+//  Created by MACBOOK on 01/11/21.
+//
+
+import UIKit
+
+class HeaderCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
